581688d5f344c1cfb3bee5519d1729bbc3adfff1
xer-urg <alex.narrator@gmail.com>
2019-05-13 13:22:40 +0300

    ~включены дефайны под свою сборку ~из билд-солюшна исключена папка utils, как ненужная для сборки релиз-версии движка ~удалены пустые папки, т.к. с ними долбанный туртоиз-свн не хотел заливать коммит на долбанный гитхаб

--- build_config_defines.h
+++ build_config_defines.h
@@ -15,18 +15,18 @@
 #define KD_DETAIL_RADIUS				// alpet: включает правку дистанции отрисовки травы и плотности. Может влиять на производительность для слабых систем.
 
 // ==================================== Правки от Real Wolf ======================================= 
-//#define INV_RUCK_UNLIMITED_FIX		// Real Wolf: позволяет безболезненно использовать атрибут unlimited в теге dragdrop_bag для создания лимитного инвентаря
+#define INV_RUCK_UNLIMITED_FIX		// Real Wolf: позволяет безболезненно использовать атрибут unlimited в теге dragdrop_bag для создания лимитного инвентаря
 #define INV_NEW_SLOTS_SYSTEM			// Real Wolf, Red Virus: включает слоты.
 #define SUN_DIR_NOT_DEBUG			    // Real Wolf: отключение вывода в лог информации вида CurrentEnv.sun_dir...
-//#define ARTEFACTS_FROM_RUCK			// Real Wolf: артефакты будут работать из всего инвентаря (слотов, пояса, рюкзака).
+#define ARTEFACTS_FROM_RUCK			// Real Wolf: артефакты будут работать из всего инвентаря (слотов, пояса, рюкзака).
 #define KNIFE_SPRINT_FIX				// Real Wolf: остановка спринта при ударе2.
 #define KNIFE_SPRINT_MOTION				// Real Wolf: вопроизведение анимации бега для ножа.
 #define MISSILE_THREAT_FIX				// Real Wolf: остановка спринта при броске гранаты, болта и т.д.
-//#define GRENADE_FROM_BELT				// Real Wolf: гранаты с пояса.
-//#define LOCK_RELOAD_IN_SPRINT			// Real Wolf: блокировка перезарядки по время спринта.
+#define GRENADE_FROM_BELT				// Real Wolf: гранаты с пояса.
+#define LOCK_RELOAD_IN_SPRINT			// Real Wolf: блокировка перезарядки по время спринта.
 #define HIDE_WEAPON_IN_CAR				// Real Wolf: прятать все оружие в машине.
 //#define BM16_ANIMS_FIX					// Real Wolf: расширение анимаций для BM16.
-//#define INV_OUTFIT_FULL_ICON_HIDE		// Real Wolf: иконка игрока в костюме заменена на иконку костюма.
+#define INV_OUTFIT_FULL_ICON_HIDE		// Real Wolf: иконка игрока в костюме заменена на иконку костюма.
 #define UI_LOCK_PDA_WITHOUT_PDA_IN_SLOT // Real Wolf: блокировать работу ПДА, если самого предмета нету в слоте.
 #define CAR_SAVE_FUEL					// Real Wolf: сохранение текущего объема топлива, максимального объема и потребления машины.
 //#define R1_EXCLUDE						// Real Wolf: отключает первый рендер, оставляя только полное динамическое освещение.
@@ -42,5 +42,5 @@
 #endif
 
 // ==================================== Правки от Karlan ======================================= 
-//#define AMMO_FROM_BELT					// Karlan: Патроны используются только с пояса
+#define AMMO_FROM_BELT					// Karlan: Патроны используются только с пояса
 
