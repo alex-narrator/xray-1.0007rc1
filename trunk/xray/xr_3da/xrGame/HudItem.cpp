ff31209c71c185f184735de1c98b3e9e079d5983
xer-urg <alex.narrator@gmail.com>
2022-04-11 22:00:59 +0300

    +правильна позиція худових звуків
    Джерело: https://github.com/OGSR/OGSR-Engine/commit/b55d2ebaff081926cfc929827c058ab40595cfb9

--- trunk/xray/xr_3da/xrGame/HudItem.cpp
+++ trunk/xray/xr_3da/xrGame/HudItem.cpp
@@ -128,7 +128,9 @@ void CHudItem::net_Destroy()
 
 void CHudItem::PlaySound	(HUD_SOUND& hud_snd, const Fvector& position, bool overlap)
 {
-	HUD_SOUND::PlaySound	(hud_snd, position, object().H_Root(), !!GetHUDmode(), false, overlap);
+	//HUD_SOUND::PlaySound	(hud_snd, position, object().H_Root(), !!GetHUDmode(), false, overlap);
+	Fvector pos = GetHUDmode() ? Fvector().sub(position, ::Sound->listener_position()) : position;
+	HUD_SOUND::PlaySound(hud_snd, pos, object().H_Root(), !!GetHUDmode(), false, overlap);
 }
 
 BOOL  CHudItem::net_Spawn	(CSE_Abstract* DC) 
